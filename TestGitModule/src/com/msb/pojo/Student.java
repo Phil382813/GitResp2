c0dabb0baec65ba7c62cc026d630dae62c783164
dqc <dqc382813.good@163.com>
2022-12-02 16:39:30 +0800

    对Student类，新增JavaBean

--- TestGitModule/src/com/msb/pojo/Student.java
+++ TestGitModule/src/com/msb/pojo/Student.java
@@ -5,4 +5,28 @@ import java.io.Serializable;
 public class Student implements Serializable {
     private int age;
     private String name;
+
+    public Student() {
+    }
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
